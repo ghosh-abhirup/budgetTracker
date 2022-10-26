35ce6964302ac703c2ae15238bcc40e2d83bda64
Abhirup <ghoshabhirup2001@gmail.com>
2022-10-26 22:57:03 +0530

    Created basic context

---
+++ src/context/context.js
@@ -0,0 +1,27 @@
+import React, { createContext } from "react";
+import { useReducer } from "react";
+import contextReducer from "./contextReducer";
+
+const initialState = [];
+export const BudgetTrackerContext = createContext(initialState);
+
+export const Provider = (props) => {
+  const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+  //Action creater
+  const deleteTransaction = (id) => {
+    dispatch({ type: "delete", payload: id });
+  };
+
+  const addTransaction = (transaction) => {
+    dispatch({ type: "add", payload: transaction });
+  };
+
+  return (
+    <BudgetTrackerContext.Provider
+      value={{ deleteTransaction, addTransaction }}
+    >
+      {props.children}
+    </BudgetTrackerContext.Provider>
+  );
+};
---
+++ src/context/contextReducer.js
@@ -0,0 +1,5 @@
+// Reducer is a function that takes in the old state and an action =>  returns a new state
+
+const contextReducer = (state, action) => {};
+
+export default contextReducer;
